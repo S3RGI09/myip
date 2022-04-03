e97a5a599caba94a723b6d02cb96c431bb3f73a2
Sergio Casero Verdial <96842235+S3RGI09@users.noreply.github.com>
2022-04-03 22:38:09 +0200

    Update myip.py

--- myip.py
+++ myip.py
@@ -3,6 +3,15 @@
 
 import socket
 
+
+print(" __  __       ___ ____ ")
+print("|  \/  |_   _|_ _|  _ \ ")
+print("| |\/| | | | || || |_) |")
+print("| |  | | |_| || ||  __/ ")
+print("|_|  |_|\__, |___|_|    ")
+print("        |___/           ")
+
+
 hostname = socket.gethostname()
 ip = socket.gethostbyname(hostname)
 print("Creada por S3RGI09 (Sergio Casero Verdial)")
