0758b3966247135113e77baa2bcf0091e224b811
Sergio Casero Verdial <96842235+S3RGI09@users.noreply.github.com>
2022-04-03 17:54:14 +0200

    Update myip.py

--- myip.py
+++ myip.py
@@ -5,7 +5,7 @@ import socket
 
 hostname = socket.gethostname()
 ip = socket.gethostbyname(hostname)
-
+print("Creada por S3RGI09 (Sergio Casero Verdial)")
 print("--------------------------------------")
 print("El nombre de tu ordenador es: " + hostname)
 print("Tu direccion IP es: " + ip)
